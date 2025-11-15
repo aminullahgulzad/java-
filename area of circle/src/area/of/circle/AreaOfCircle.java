5244ec01ce562b4c8b1789c2c9c6e59323c59f6b
aminullahgulzad <AminGulzad9264@gmail.com>
2025-11-15 15:13:08 +0430

    Add files via upload

---
+++ area of circle/src/area/of/circle/AreaOfCircle.java
@@ -0,0 +1,21 @@
+
+package area.of.circle;
+
+public class AreaOfCircle {
+
+   
+    public static void main(String[] args) {
+        // to the find area of circle
+     double radius;
+     double area;
+     
+     radius=3;
+     area=radius*radius*3.14;
+     System.out.println("the area of this circle is"+area);
+             
+        
+        
+        
+    }
+    
+}
